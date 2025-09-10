193f9396c39fac482541ab1e498e689891019e68
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-10 14:16:08 -0400

    added display alert &  settimeout in catch block

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -9,11 +9,19 @@ if (document.querySelector('#new-pet')) {
         }
 
         axios.post('/pets', pet)
-            .then(function (response) {
-                window.location.replace(`/pets/${response.data.pet._id}`);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        .then(function (response) {
+          window.location.replace(`/pets/${response.data._id}`);
+        })
+        .catch(function (error) {
+          const alert = document.getElementById('alert')
+          alert.classList.add('alert-warning');
+          alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
+          alert.style.display = 'block';
+
+          setTimeout(() => {
+            alert.style.display = 'none';
+            alert.classList.remove('alert-warning');
+           }, 3000)
+        });
     });
 }
