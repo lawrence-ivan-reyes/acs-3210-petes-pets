af66ed0b031806668dd163812d6cdb449d2ce23b
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-17 14:41:51 -0400

    implemented responding to json

--- test/test-pets.js
+++ test/test-pets.js
@@ -35,6 +35,19 @@ describe('Pets', ()  => {
         });
   });
 
+  // TEST JSON API
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
+
   // TEST NEW
   it('should display new form on /pets/new GET', (done) => {
     chai.request(server)
