eaae6302da6d01f5480e5f8b61c7b387673849b1
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-17 14:04:27 -0400

    implemented stripe payment

--- routes/pets.js
+++ routes/pets.js
@@ -43,10 +43,17 @@ module.exports = (app) => {
   app.post('/pets', upload.single('avatar'), (req, res, next) => {
     var pet = new Pet(req.body);
     pet.save(function (err) {
+        if (err) {
+            console.error("Validation error:", err);
+            return res.status(400).send({ err: err });
+          }
+
       if (req.file) {
         // Upload the images
         client.upload(req.file.path, {}, function (err, versions, meta) {
-          if (err) { return res.status(400).send({ err: err }) };
+          if (err) { 
+            console.error("Upload error:", err);
+            return res.status(400).send({ err: err }) };
 
           // Pop off the -square and -standard and just use the one URL to grab the image
           versions.forEach(function (image) {
@@ -54,10 +61,15 @@ module.exports = (app) => {
             urlArray.pop();
             var url = urlArray.join('-');
             pet.avatarUrl = url;
-            pet.save();
           });
 
-          res.send({ pet: pet });
+          pet.save(function (err) {
+            if (err) {
+              console.error('Pet save failed:', err);
+              return res.status(400).send({ err });
+            }
+            return res.send({ pet: pet });
+          });
         });
       } else {
         res.send({ pet: pet });
@@ -113,5 +125,40 @@ module.exports = (app) => {
       { page: page }).then((results) => {
         res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
       });
+  });
+
+  // PURCHASE
+  app.post('/pets/:id/purchase', (req, res) => {
+    console.log(req.body);
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+    var stripe = require("stripe")(process.env.PRIVATE_STRIPE_API_KEY);
+
+    // Token is created using Checkout or Elements!
+    // Get the payment token ID submitted by the form:
+    const token = req.body.stripeToken; // Using Express
+
+    // req.body.petId can become null through seeding,
+    // this way we'll insure we use a non-null value
+    let petId = req.body.petId || req.params.id;
+
+    Pet.findById(petId).exec((err, pet)=> {
+      if (err) {
+        console.log('Error: ' + err);
+        res.redirect(`/pets/${req.params.id}`);
+      }
+      const charge = stripe.charges.create({
+        amount: pet.price * 100,
+        currency: 'usd',
+        description: `Purchased ${pet.name}, ${pet.species}`,
+        source: token,
+      }).then((chg) => {
+        res.redirect(`/pets/${req.params.id}`);
+      })
+      .catch(err => {
+        console.log('Error:' + err);
+      });
+    })
+
   });
 }
--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Pass Stripe API key to templates
+app.locals.PUBLIC_STRIPE_API_KEY = process.env.PUBLIC_STRIPE_API_KEY
 
 require('./routes/index.js')(app);
 require('./routes/pets.js')(app);
