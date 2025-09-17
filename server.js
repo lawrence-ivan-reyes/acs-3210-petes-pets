b150e6120653972ab4ed61c4f93560f4c0b50ca4
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-17 14:33:52 -0400

    implemented emails

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
 
+// require our mailgun dependencies
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
 const app = express();
 
 const mongoose = require('mongoose');
@@ -19,6 +23,39 @@ mongoose.connect('mongodb://localhost/petes-pets', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
+});
+
+// auth with our mailgun API key and domain
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.EMAIL_DOMAIN
+  }
+}
+
+// create a mailer
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+// SEND EMAIL
+const user = {
+  email: 'lawrence.reyes@students.dominican.edu',
+  name: 'Ivan',
+  age: '100'
+};
+
+nodemailerMailgun.sendMail({
+  from: `no-reply@${process.env.EMAIL_DOMAIN}`,
+  to: user.email, // An array if you have multiple recipients.
+  subject: 'Hey you, awesome!',
+  template: {
+    name: 'email.handlebars',
+    engine: 'handlebars',
+    context: user
+  }
+}).then(info => {
+  console.log('Response: ' + info);
+}).catch(err => {
+  console.log('Error: ' + err);
 });
 
 // view engine setup
